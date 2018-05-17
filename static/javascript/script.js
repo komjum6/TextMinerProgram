04dab7ba18ce1f3a32493901b359dc8011a92799
Kahuub <HCR.Goltstein@student.han.nl>
2018-05-17 17:40:56 +0200

    Hopelijk de correcte versies voor cytosine erop gezet en niks gesloopt

---
+++ static/javascript/script.js
@@ -0,0 +1,97 @@
+function litteShit() {
+    
+
+    var svg = d3.select("svg"),
+    width = +svg.attr("width"),
+    height = +svg.attr("height");
+
+var color = d3.scaleOrdinal(d3.schemeCategory20);
+
+var simulation = d3.forceSimulation()
+    .force("link", d3.forceLink().id(function(d) { return d.id; }).distance(100))
+	//.force("link", d3.forceLink().id(function (d) {return d.id;}).distance(100).strength(1))
+    .force("charge", d3.forceManyBody(100))
+    .force("center", d3.forceCenter(width / 2, height / 2));
+
+var abs_url = "http://cytosine.nl/~owe7_pg3/realz"
+var rel_url = "/static/javascript/miserables.json"
+var url = abs_url + rel_url
+
+d3.json(url, function(error, graph) {
+  if (error) throw error;
+
+   var link = svg.append("g")
+      .attr("class", "links")
+    .selectAll("line")
+    .data(graph.links)
+    .enter().append("line")
+      .attr("stroke-width", function(d) { return Math.sqrt(d.value); });
+
+  var node = svg.append("g")
+      .attr("class", "nodes")
+    .selectAll("g")
+    .data(graph.nodes)
+    .enter().append("g")
+    
+  var circles = node.append("circle")
+      .attr("r", 10)
+      .attr("fill", function(d) { return color(d.group); })
+      .call(d3.drag()
+          .on("start", dragstarted)
+          .on("drag", dragged)
+          .on("end", dragended))
+	  .on("click", function(d) { nodeClick(d); } );
+		  
+
+  var lables = node.append("text")
+      .text(function(d) {
+        return d.id;
+      })
+      .attr('x', 6)
+      .attr('y', 3);
+
+  node.append("title")
+      .text(function(d) { return d.id; });
+
+  simulation
+      .nodes(graph.nodes)
+      .on("tick", ticked);
+
+  simulation.force("link")
+      .links(graph.links);
+
+   function ticked() {
+    link
+        .attr("x1", function(d) { return d.source.x; })
+        .attr("y1", function(d) { return d.source.y; })
+        .attr("x2", function(d) { return d.target.x; })
+        .attr("y2", function(d) { return d.target.y; });
+
+    node
+        .attr("transform", function(d) {
+          return "translate(" + d.x + "," + d.y + ")";
+        })
+  }
+});
+
+function nodeClick( node ) {
+     d3.select('h1').text(node.id)
+    }
+
+function dragstarted(d) {
+  if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+  d.fx = d.x;
+  d.fy = d.y;
+}
+
+function dragged(d) {
+  d.fx = d3.event.x;
+  d.fy = d3.event.y;
+}
+
+function dragended(d) {
+  if (!d3.event.active) simulation.alphaTarget(0);
+  d.fx = null;
+  d.fy = null;
+}
+}
