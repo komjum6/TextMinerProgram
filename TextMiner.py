aa2832e011fcb813bc19d542b85cb054d2323561
Bertram <31915897+BertramHutten@users.noreply.github.com>
2018-05-23 12:52:49 +0200

    Add files via upload

--- TextMiner.py
+++ TextMiner.py
@@ -6,27 +6,19 @@ app = Flask(__name__)
 #De Root Map
 @app.route('/')
 def index():
-    return render_template('home.html')
+    return render_template('index.html')
     
 #De plaats waar dingen gevisualiseerd worden
-@app.route('/visualisations')
-def visualisations():
-    return render_template('visualisations.html')
+@app.route('/visualisation')
+def visualisation():
+    return render_template('visualisation.html')
 
-#De plaats voor sidenotes
-@app.route('/notes')
-def notes():
-    return render_template('notes.html')
+#De plaats voor contact info
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
 
 #Een plaats voor een introductie etc
 @app.route('/about')
 def about():
     return render_template('about.html')
-
-#Error handling
-@app.errorhandler(404)
-def page_not_found(error):
-    return render_template('page_not_found.html'), 404
-
-if __name__ == '__main__':
-    app.run(debug=True)
